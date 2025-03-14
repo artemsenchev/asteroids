4f74c325ff4908efc7d011582c392785d61de7db
artemsenchev <asenchev@gmail.com>
2025-03-14 16:12:08 -0700

    Added functionality to render player & take in input keys A & D for rotation of player

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
 
+    pygame.display.set_caption("Asteroids")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +20,17 @@ def main():
                 return
 
         screen.fill((0, 0, 0))  # Clear the screen with black
-        pygame.display.flip() 
+
+        # Update the game state
+        player.draw(screen)
+        player.update(dt)
+
+        pygame.display.flip() #flip/update the display: make the drawn content visible
+
+        
+
+        time_passed = game_clock.tick(fps)  # Maintain the game at a constant FPS
+        dt = time_passed / 1000.0  # Convert milliseconds to seconds
 
 
 
